ccbbf79056cc0179eb3aaa597c53f28d0d59af4d
kate-lobatenko <ttt1232444@gmail.com>
2019-04-10 16:08:52 +0300

    added data for cells container

--- app.js
+++ app.js
@@ -1,27 +1,61 @@
+window.addEventListener('load', function () {
+  new init;
+});
+
 function init() {
-  drawCells();
+  drawContainer();
+  setCells();
 }
 
-function getCells(cellsQuanity) { 
-  return cellsQuanity;
-}
+function drawContainer() {
 
-function drawCells() {
-
-  const cellsQuanity = getCells(500);
+  const CELLS_QUANTITY = getCellsQuantity();
 
   const $container = document.getElementById("container");
 
-
-  for (let i = 0; i < cellsQuanity; i++) {
+  for (let i = 0; i < CELLS_QUANTITY; i++) {
     let $cell = document.createElement("div");
-    $cell.textContent = i;
     $cell.classList = "cell";
     $container.appendChild($cell);
   }
-
 }
 
-window.addEventListener('load', function () {
-  new init();
-});
+function getCellsQuantity() {
+  const CELLS_QUANTITY = 400;
+  return CELLS_QUANTITY;
+}
+
+function setCells() {
+  const CELLS_QUANTITY = getCellsQuantity();
+  const startCell = 0;
+  const endCell = CELLS_QUANTITY - 2;
+  const walls = [5,7,9,25,36,50,51,52,53,108,115];
+  const rowsQuantity = 20;
+  const columnsQuantity = 20;
+
+  let rows = columns = [];
+
+  for (let i = 0; i < rowsQuantity; i++) {
+    rows[i] = 0;
+  }
+
+  let cells = new Array(rows, columns);
+
+
+  let $cellContainers = document.getElementsByClassName("cell");
+  for (i in $cellContainers) {    
+      if(i == startCell){
+        $cellContainers[i].className += " startCell";
+      }
+
+      if(i == endCell){
+        $cellContainers[i].className += " endCell";
+      }
+
+      if(walls.includes(Number(i))) {
+        console.log("wall is ", i);
+        $cellContainers[i].className += " wall";
+      }
+  }
+
+}
